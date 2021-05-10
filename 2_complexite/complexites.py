f8f604ea84ba3e5ce1edb167cb5f56df5d1fb238
Martin Digard <martindigard@gmail.com>
2021-05-10 23:17:48 +0200

    complexites.py

--- 2_complexite/complexites.py
+++ 2_complexite/complexites.py
@@ -5,9 +5,6 @@
 Calculer la complexité en temps et en espace.
 """
 
-import os
-import matplotlib.pyplot as plt
-from scipy import stats
 import argparse
 import glob
 import re
@@ -71,7 +68,7 @@ def nettoyage_qtt(qtt, unite):
                 qtt_ingr += 1
     else:
         qtt_ingr += 1.
-    return qtt_ingr 
+    return qtt_ingr
 
 
 def annotation_qtt(recette, infos_ingr):
@@ -80,22 +77,24 @@ def annotation_qtt(recette, infos_ingr):
     """
     if len(infos_ingr) != 0:
         if len(infos_ingr[0].split('\t')) < 3:
-            return annotation_qtt(infos_ingr[1:])
+            return annotation_qtt(recette, infos_ingr[1:])
         qtt, unite, ingr = infos_ingr[0].split('\t')
         qtt = nettoyage_qtt(qtt, unite)
-        if float(int(qtt)) != float(qtt): 
+        if float(int(qtt)) != float(qtt):
             qtt = int(qtt) + 1
         else:
             qtt = int(qtt)
 
         for token in ingr.split():
             if len(token) > 2:
-                recette = re.sub(f'<ingredient>([^<]*{token}[^<]*)</ingredient>', f'<ingredient quantite={qtt}>\\1</ingredient>', recette)
+                recette = re.sub(f'<ingredient>([^<]*{token}[^<]*)'
+                                 '</ingredient>',
+                                 f'<ingredient quantite={qtt}>\\1'
+                                 '</ingredient>',
+                                 recette)
         return annotation_qtt(recette, infos_ingr[1:])
-    # Marche pas !?
     recette = re.sub('<ingredient>', '<ingredient quantite=1>', recette)
     return recette
-
 
 
 def decompte_ingredients(infos_ingr, nb_ingr):
@@ -201,10 +200,9 @@ def main():
 
     nb_ingr_oper_all = []
     compteur = 0
-    # if os.path.exists('recettes_problematiques'):
-    #     os.remove('recettes_problematiques')
     for fichier in glob.glob(args.rep_corpus_annote + '*/*'):
-        print(f"\n\n==> {compteur}\nTraitement de la recette {fichier} en cours…\n")
+        print(f'\n\n==> {compteur}\nTraitement de la recette {fichier} en '
+              'cours…\n')
         compteur += 1
         if compteur > 100:
             break
@@ -223,96 +221,30 @@ def main():
             ingr = block.split('\n')[1:]
             infos_ingr[nom] = ingr
 
-        # for k,v in infos_ingr.items():
-        #     print(k)
-        #     for elt in v:
-        #         print(elt)
-        #     print()
-
         # Nom du fichier pour la lecture de la clef du dico
         fichier = fichier.split('/')[-1]
 
         # Annotation des balises ingrédients
         qtt_annotee = annotation_qtt(recette_annotee, infos_ingr[fichier])
         print(qtt_annotee)
-        
+
         qtt_ingredients = decompte_ingredients(infos_ingr[fichier], 0)
+        print(type(qtt_ingredients))
 
         nb_ingredients = len(infos_ingr[fichier])
 
         if nb_ingredients > 20:
             recettes_problematiques.append((fichier, nb_ingredients))
-        #     continue
-        
-        # print(f"Nombre d’ingrédients : {nb_ingredients}")
-
 
         # L’association du temps aux opérations
         oper_temps = temps(prepa_complexite[0], refs_tps)
-        # for elt in oper_temps:
-        #     print(elt)
 
-        # print(f"Nombre d’opérations de base : {len(oper_temps)}")
-        
         nb_recipients = len(prepa_complexite[1]) + len(infos_ingr[fichier])
-        # print(f"Nombre total de récipients pour cette recette : {nb_recipients}\n\n")
-
-        # with open('recettes_problematiques', 'a') as rp:
-        #     rp.write(f"{fichier}\ningr : {nb_ingredients}\noper : {len(oper_temps)}\nrecip : {nb_recipients}\n\n")
+        print(type(nb_recipients))
 
         nb_ingr_oper_all.append((nb_ingredients, len(oper_temps)))
+
 
 if __name__ == "__main__":
 
     main()
-
-
-###############################################################################
-
-
-# Calcul du temps de recettes
-# somme = sum([tps for ope, tps in oper_temps])
-# moyenne = 0
-# if len(oper_temps) != 0:
-#     moyenne = somme / len(oper_temps)
-
-
-# print("Durée de préparation :")
-# print(("\t- Par somme des temps des chaque opérations : "),
-#       (f"{round(somme, 2)}"))
-# print("\t- Complexité en temps : ???\n")
-# print()
-
-# # Stockage de tous les nombres
-#     nb_ingr_oper_all.append((len(prepa_complexite[0]),
-#                              len(prepa_complexite[1])))
-# moyenne_ingr_all = sum(
-#         [elt[0] for elt in nb_ingr_oper_all]) / len(nb_ingr_oper_all)
-# moyenne_oper_all = sum(
-#         [elt[1] for elt in nb_ingr_oper_all]) / len(nb_ingr_oper_all)
-# print(f"\n{moyenne_ingr_all}")
-# print(moyenne_oper_all)
-    # # HYPOTHÈSE DE LINÉARITÉ
-
-    # # Générer les axes
-    # x = [elt[0] for elt in nb_ingr_oper_all]
-    # y = [elt[1] for elt in nb_ingr_oper_all]
-
-    # # Dessiner un nuage de points
-    # plt.scatter(x, y)
-    # plt.show()
-
-    # # Dessiner la ligne de régression linéaire
-    # slope, intercept, r, p, std_err = stats.linregress(x, y)
-
-    # def myfunc(x_axis):
-    #     """ Calcul de la régression linéaire ?? """
-    #     return slope * x_axis + intercept
-
-    # mymodel = list(map(myfunc, x))
-    # plt.scatter(x, y)
-    # plt.plot(x, mymodel)
-    # plt.show()
-
-    # print(f"stp_err : {std_err}")
-    # print(f"r : {r}")
