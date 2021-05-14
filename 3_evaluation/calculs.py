037923b78b7f36b3e4d452e67fc90f6a2dbb848d
anaelle-p <anaellepierredon@gmail.com>
2021-05-14 22:46:19 +0200

    correlation anaelle

--- 3_evaluation/calculs.py
+++ 3_evaluation/calculs.py
@@ -12,9 +12,50 @@ Programme principal de ce répertoire :
 import argparse
 from glob import glob
 import re
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Modules du projet
 import evaluation as ev
+
+
+def recup_temps_espace(recette):
+    """
+    Calcul le temps total nécessaire pour une recette.
+    """
+    tous_les_temps = [float(tps) for tps in re.findall('<operation temps=(.*?)min', recette)]
+    test_espace = re.search('espace=(.*?)>', recette)
+    if test_espace:
+        espace = test_espace.group(1)
+    else:
+        espace = 1
+    return sum(tous_les_temps), float(espace)
+
+
+def correlation(liste_de_tuple):
+    """
+    """
+    niveau = [var[0] for var in liste_de_tuple]
+    temps = [var[1] for var in liste_de_tuple]
+    espace = [var[2] for var in liste_de_tuple]
+
+    plt.scatter(niveau, temps)
+    plt.title("Calcul de corrélation entre le niveau et le temps")
+    plt.xlabel('temps')
+    plt.ylabel('niveau')
+    plt.show()
+
+    plt.scatter(niveau, espace)
+    plt.title("Calcul de corrélation entre le niveau et le temps")
+    plt.xlabel('espace')
+    plt.ylabel('niveau')
+    plt.show()
+
+    print(np.corrcoef(niveau, temps)[0,1])
+    print(np.corrcoef(niveau, espace)[0,1])
+
+    plt.title("Calcul de corrélation")
+
 
 
 def main():
@@ -26,10 +67,12 @@ def main():
     parser.add_argument('rep_corpus_final')
     args = parser.parse_args()
     liste_valeurs_locales = []
+    axes = []
+
     for chemin_fichier in glob(args.rep_corpus_final + '/*'):
         with open(chemin_fichier) as recette_finale:
             recette = recette_finale.read()
-
+        print(chemin_fichier)
         # Récupérer le contenu des balises ingrédients
         ingredients_corps = re.findall(r'<ingredient[^>]+>(.*?)</ingredient>',
                                        recette)
@@ -47,7 +90,31 @@ def main():
         # Préparer les évaluations globales
         liste_valeurs_locales.append((vpos, fpos, fneg))
 
+        # Les niveaux de recettes pour le calcul de corrélation
+        niveau = re.search('<niveau>(.*)</niveau>', recette).group(1)
+        print(niveau)
+
+        # Additionner les temps
+        temps_total, espace_total = recup_temps_espace(recette)
+
+        # Préparation du tuple
+        if niveau == "Très facile":
+            niveau = 1
+        if niveau == "Facile":
+            niveau = 2
+        if niveau == "Moyennement difficile":
+            niveau = 3
+        if niveau == "Difficile":
+            niveau = 4
+
+        axes.append((round(niveau, 3), round(temps_total, 3),
+                     round(espace_total, 3)))
+
+    # Calcul de l'évaluation
     ev.evaluation_globale(liste_valeurs_locales)
+
+    # Calcul des corrélations
+    correlation(axes)
 
 
 if __name__ == "__main__":
